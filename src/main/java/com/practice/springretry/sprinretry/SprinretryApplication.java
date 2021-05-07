921233a62fcbc6b4cb37097f40332a6b20c2b93e
sanath.bt <sanath.bt@tavant.com>
2021-05-07 08:22:15 +0530

    Spring retry inital commit

---
+++ src/main/java/com/practice/springretry/sprinretry/SprinretryApplication.java
@@ -0,0 +1,13 @@
+package com.practice.springretry.sprinretry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinretryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinretryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/springretry/sprinretry/SprinretryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.springretry.sprinretry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinretryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
